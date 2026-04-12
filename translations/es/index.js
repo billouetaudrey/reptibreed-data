6c5c7d277a782202afb0b0de04855af9c177c4eb
Billou <32905881+billouetaudrey@users.noreply.github.com>
2026-04-12 13:23:17 +0200

    Update translations/es/index.js from CLI deploy script

---
+++ translations/es/index.js
@@ -0,0 +1,13 @@
+import common from './common';
+import screens from './screens';
+import settings from './settings';
+import species from './species';
+import other from './other';
+
+export const es = {
+  ...common,
+  ...screens,
+  ...settings,
+  ...species,
+  ...other
+};
