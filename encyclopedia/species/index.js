0af87c68f3831ec9363520541c3eb54555808318
Beingmishra <rahul731999@gmail.com>
2026-04-23 13:48:45 +0200

    📝 Mise à jour Encyclopédie (2026-04-23_13:48)

--- encyclopedia/species/index.js
+++ encyclopedia/species/index.js
@@ -1,15 +1,10 @@
-const extract = (data) => {
-    if (!data) return [];
-    return (data.default !== undefined) ? data.default : data;
-};
-
-const snakes = extract(require('./snakes.json'));
-const lizards = extract(require('./lizards.json'));
-const turtles = extract(require('./turtles.json'));
-const amphibians = extract(require('./amphibians.json'));
-const crocodilians = extract(require('./crocodilians.json'));
-const arachnids = extract(require('./arachnids.json'));
-const invertebrates = extract(require('./invertebrates.json'));
+const snakes = require('./snakes.json');
+const lizards = require('./lizards.json');
+const turtles = require('./turtles.json');
+const amphibians = require('./amphibians.json');
+const crocodilians = require('./crocodilians.json');
+const arachnids = require('./arachnids.json');
+const invertebrates = require('./invertebrates.json');
 
 module.exports = [
     ...snakes,
