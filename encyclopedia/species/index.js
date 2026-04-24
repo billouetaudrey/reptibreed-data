b3854cc227d0b5a7a31474e536dd532270926430
Beingmishra <rahul731999@gmail.com>
2026-04-24 08:23:04 +0200

    📝 Mise à jour Encyclopédie (2026-04-24_08:23)

--- encyclopedia/species/index.js
+++ encyclopedia/species/index.js
@@ -1,5 +1,6 @@
 const snakes = require('./snakes.json');
 const lizards = require('./lizards.json');
+const varanidae = require('./varanidae.json');
 const turtles = require('./turtles.json');
 const amphibians = require('./amphibians.json');
 const crocodilians = require('./crocodilians.json');
@@ -9,6 +10,7 @@ const invertebrates = require('./invertebrates.json');
 module.exports = [
     ...snakes,
     ...lizards,
+    ...varanidae,
     ...turtles,
     ...amphibians,
     ...crocodilians,
